79984b822a5b47b12394fe22f2d4b61a1a43adb6
CoreyRecord <73188618+CoreyRecord@users.noreply.github.com>
2020-12-01 21:15:16 -0500

    IntegrationProjectFinal

--- IntegrationProject.py
+++ IntegrationProject.py
@@ -1,23 +1,64 @@
-#Corey Record
-#Description: This program will be a calorie tracker that will help you either lose or gain weight based on your goals.
+"""__author__ = "Corey Record" """
+
+
+# Description: This program will be a calorie tracker that will help you either
+# lose or gain weight based on your goals.
+
 
 def main():
-
-    #Input initial variables
+    """ Input initial variables """
     print("Welcome to the app that will help you meet your caloric goal!")
     print()
-    weight = float(input("What is your starting weight in pounds? "))
-    height = int(input("What is your height in inches? "))
+    while True:
+        try:
+            weight = float(input("What is your starting weight in pounds? "))
+        except ValueError:
+            print("Please enter an integer.")
+            continue
+        else:
+            break
+    while True:
+        try:
+            height = int(input("What is your height in inches? "))
+        except ValueError:
+            print("Please enter an integer.")
+            continue
+        else:
+            break
     gender = str(input("What is your gender? male/female "))
-    age = int(input("What is your age? "))
+    while True:
+        if gender == "male":
+            break
+        elif gender == "female":
+            break
+        else:
+            print("Please enter male or female")
+            gender = str(input("What is your gender? male/female "))
+    while True:
+        try:
+            age = int(input("What is your age? "))
+        except ValueError:
+            print("Please enter an integer.")
+            continue
+        else:
+            break
     goal = str(input("What is your goal? gain/lose/maintain "))
+    while True:
+        if goal == "gain":
+            break
+        elif goal == "lose":
+            break
+        elif goal == "maintain":
+            break
+        else:
+            print("Please enter gain, lose, or maintain")
+            goal = str(input("What is your goal? gain/lose/maintain "))
     print()
-    bmr = 0   
-   
+    bmr = 0
 
-    #run if statement if gender is male
+    # run if statement if gender is male
     if gender == "male":
-        #calculate basal metabolic rate based on goal
+        # calculate basal metabolic rate based on goal
         if goal == "lose":
             bmr = ((10 * weight) + (6.25 * height) - (5 * age)) - 200
         if goal == "maintain":
@@ -25,79 +66,122 @@ def main():
         if goal == "gain":
             bmr = ((10 * weight) + (6.25 * height) - (5 * age)) + 200
 
-        #run if statement if gender is female
+        # run if statement if gender is female
     if gender == "female":
-        #calculate basal metabolic rate based on goal
+        # calculate basal metabolic rate based on goal
         if goal == "lose":
-            bmr = ((10 * weight) + (6.25 * height) - (5 * age) -161) - 200
+            bmr = ((10 * weight) + (6.25 * height) - (5 * age) - 161) - 200
         if goal == "maintain":
-            bmr = ((10 * weight) + (6.25 * height) - (5 * age) -161)
+            bmr = ((10 * weight) + (6.25 * height) - (5 * age) - 161)
         if goal == "gain":
-            bmr = ((10 * weight) + (6.25 * height) - (5 * age) -161) + 200
-            
+            bmr = ((10 * weight) + (6.25 * height) - (5 * age) - 161) + 200
+
     print("Daily calorie goal is ", format(bmr, ".0f"), " calories", sep="")
     print()
 
-    #defines how many meals 
-    meal_count = int(input("How many meals did you eat today? "))
+    # defines how many meals
+    while True:
+        try:
+            meal_count = int(input("How many meals did you eat today? "))
+        except ValueError:
+            print("Please enter a integer.")
+            continue
+        else:
+            break
     total = 0
 
-    #takes the amount of meals and adds the calories
+    # takes the amount of meals and adds the calories
     for x in range(meal_count):
-        meal_cal = int(input("How many calories were in one of your meals? "))
-        total += meal_cal
-    #calculate basal metabolic rate based on goal
-    if total >= (bmr - 100) and total <= (bmr + 100):
+        while True:
+            try:
+                meal_cal = int(
+                    input("How many calories were in one of your meals? "))
+                total += meal_cal
+            except ValueError:
+                print("Please enter an integer.")
+                continue
+            else:
+                break
+    # calculate basal metabolic rate based on goal
+    # if total >= (bmr - 100) and total <= (bmr + 100): This is how it would be
+    # written in most languages
+    if (bmr - 100) <= total <= (bmr + 100):
         print("You have eaten in the calorie goal")
     if total < (bmr - 100):
-        print("You have eaten in a calorie defecit")
+        print("You have eaten in a calorie deficit")
     if total > (bmr + 100):
         print("You have eaten in a calorie surplus")
 
     print()
-    
-    #defines if cardio is performed
+
+    # defines if cardio is performed
     cardio = str(input("Did you perform cardio? yes/no "))
-    caloriesBurned = 0
-    #run if cardio is yes
-    if cardio == "yes":
-        #calculates the calories burned based on type of cardio and how much 
-        cardiotype = str(input("What type of cardio? run/walk/cycle "))
-        if cardiotype == "run":
-            miles = float(input("How many miles did you run? "))
-            caloriesBurned = miles * 170  
-        elif cardiotype == "walk":
-            miles = float(input("How many miles did you walk? "))
-            calroiesBurned = miles * 100 
-        elif cardiotype == "cycle":
-            miles = float(input("How many miles did you cycle? "))
-            caloriesBurned = miles * 60
-        print("You have burned ", format(caloriesBurned, ".0f"), " calories.", sep="")
+    calories_burned = 0
+    while True:
+        if cardio == "yes":
+            break
+        elif cardio == "no":
+            break
+        else:
+            print("Please enter yes or no")
+            cardio = str(input("Did you perform cardio? yes/no "))
+    # run if cardio is yes
+    while True:
+        if cardio == "yes":
+            # calculates the calories burned based on type of cardio and how
+            # much
+            cardio_type = str(input("What type of cardio? run/walk/cycle "))
+            if cardio_type == "run":
+                miles = float(input("How many miles did you run? "))
+                calories_burned = miles * 170
+            elif cardio_type == "walk":
+                miles = float(input("How many miles did you walk? "))
+                calories_burned = miles * 100
+            elif cardio_type == "cycle":
+                miles = float(input("How many miles did you cycle? "))
+                calories_burned = miles * 60
+            else:
+                print("Please enter run, walk, or cycle")
+                # cardio = str(input("Did you perform cardio? yes/no "))
+                print("You have burned ", format(calories_burned, ".0f"),
+                      " calories.",
+                      sep="")
 
-        print()
-        
-    #run if cardio is no 
-    if cardio == "no":
-        print("Consider doing cardio to help with your caloric goals.")
+        if cardio == "no":
+            print("Consider doing cardio to help with your caloric goals.")
+            break
 
-    #determines gender for calculation of ideal bodweight and calls the function based on that 
+    # run if cardio is no
+    # if cardio == "no":
+
+    # print("Consider doing cardio to help with your caloric goals.")
+
+    # determines gender for calculation of ideal body weight and calls the
+    # function based on that
     if gender == "male":
-        ibwm = idealBodyweightMen(height)
-        print("The ideal bodyweight for your height is: ", ibwm)
-    
+        ideal_weight_men = ideal_body_weight_men(height)
+        print("The ideal body weight for your height is: ",
+              format(ideal_weight_men, ".2f"))
+
     else:
-        ibwf = idealBodyweightFemale(height)
-        print("The ideal bodyweight for your height is: ", ibwf)
+        ideal_weight_female = ideal_body_weight_female(height)
+        print("The ideal body weight for your height is: ",
+              format(ideal_weight_female, ".2f"))
 
-#calculates the ideal bodyweight for males at a certain height    
-def idealBodyweightMen(height):
-    cmHeight = height * 30.48
-    return 50 +(0.91 *(cmHeight - 152.4))
+        # calculates the ideal body weight for males at a certain height
 
-#calculates the ideal bodyweight for females at a certain height
-def idealBodyweightFemale(height):
-    cmHeight = height * 30.48
-    return 45.5 +(0.91 *(cmHeight - 152.4))
-        
+
+def ideal_body_weight_men(height):
+    """ Creates a function that calculates male Ideal body weight"""
+    cm_height = height * 30.48
+    return 50 + (0.91 * (cm_height - 1700)) - 300
+
+
+# calculates the ideal body weight for females at a certain height
+def ideal_body_weight_female(height):
+    """ Creates a function that calculates female Ideal body weight"""
+    cm_height = height * 30.48
+    return 45.5 + (0.91 * (cm_height - 1700)) - 300
+
+
 main()
-
